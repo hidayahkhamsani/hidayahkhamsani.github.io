bd2dcd27fd81f1e1fd194f8001318a99d245e9b5
hidayah khamsani <nurulkhamsani@gmail.com>
2021-10-01 21:13:21 +0800

    2017

--- event-handler/js/events.js
+++ event-handler/js/events.js
@@ -2,6 +2,7 @@ function transformUppercase(_val){
     return _val.toUpperCase();
 }
 
+// DOM event listener
 let elNamaPengguna = document.getElementById("namaPengguna")
 elNamaPengguna.addEventListener("keyup",function(){
     elNamaPengguna.value = transformUppercase(elNamaPengguna.value)
@@ -13,10 +14,11 @@ function checkPassword(){
 
     if(pass_val.length > 5){
         alert("Password check...OK!")
-    } else{
+    } else {
         alert("Password too short!")
     }
 }
 
-let elKL = document.getElementById("KataLaluan")
+// traditional DOM event handler
+let elKL = document.getElementById("kataLaluan")
 elKL.onblur = checkPassword
