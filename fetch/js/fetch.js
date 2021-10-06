6c1baadd00eedc97065ba16a311945e9c49f51d4
hidayah khamsani <nurulkhamsani@gmail.com>
2021-10-06 21:58:21 +0800

    added

--- fetch/js/fetch.js
+++ fetch/js/fetch.js
@@ -20,3 +20,69 @@ let elLoadUser = document.getElementById("getRandomUser")
 elLoadUser.addEventListener("click",function(){
     loadUser()
 })
+
+let elLoadMultipleUsers = document.getElementById("getMultipleUser")
+elLoadMultipleUsers.addEventListener("click",function(){
+    let userCount = document.getElementById("userCount")
+    LoadMultipleUsers(userCount.value)
+})
+
+function LoadMultipleUsers(userCount){
+    let url = "https://randomuser.me/api/?results=" + userCount
+    let temp = ""
+
+    fetch(url)
+    .then((response) => response.json())
+    .then(data => {
+        let allUsers = document.getElementById("allUsers")
+        //azad.data[0].category
+        for(let i = 0; i < data.results.length; i++){
+            let fn = '<div>' + data.results[i].name.first + '</div>'
+            let ln = '<div>' + data.results[i].name.last + '</div>'
+            let phone = '<div>' + data.results[i].phone + '</div>'
+            let img = '<img src="' + data.results[i].picture.large + '">'
+
+            temp = temp + fn + ln + phone + img
+        }
+        allUsers.innerHTML = temp
+    })
+}
+
+let elLoadGender = document.getElementById("getGenderUser")
+elLoadGender.addEventListener("click",function(){
+    console.log("Load Gender clicked!")
+    //get the gender
+    let gender = document.getElementById("userGender")
+    console.log("Selected Gender: " + gender.value)
+
+    //get the number of result
+    let count = document.getElementById("userGenderCount")
+    console.log("Count: " + count.value)
+    //call loadGender() and pass gender and number of result
+    loadGender(gender.value,count.value)
+})
+
+function loadGender(gender, userCount){
+    let url = "https://randomuser.me/api/?results=" + userCount + "&gender=" + gender
+    console.log(url)
+
+    //call fetch, loop the result, and change the innerHTML for allGenderUsers
+    fetch(url)
+    .then((response) => response.json())
+    .then(data => {
+        let allGenderUsers = document.getElementById("allGenderUsers")
+        let temp = ""
+
+        for(let i = 0; i < data.results.length; i++){
+            let fn = '<div>' + data.results[i].name.first + '</div>'
+            let ln = '<div>' + data.results[i].name.last + '</div>'
+            let phone = '<div>' + data.results[i].phone + '</div>'
+            let img = '<img src="' + data.results[i].picture.large + '">'
+            let gdr = '<div>' + data.results[i].gender + '</div>'
+            temp = temp + fn + ln + phone + gender + img
+        }
+
+        
+        allGenderUsers.innerHTML = temp
+    })
+}
