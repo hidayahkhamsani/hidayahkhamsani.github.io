909210017cb541bba699250b3a681c15d5d3e9d7
hidayah khamsani <nurulkhamsani@gmail.com>
2021-10-31 23:46:09 +0800

    31/10/21

---
+++ project/js/book.js
@@ -0,0 +1,41 @@
+let bookNowBtn = document.getElementById("bookNow")
+bookNowBtn.addEventListener("click", function () {
+    let userName = document.getElementById("userName")
+    let userNameVal = userName.value
+
+    let userEmail = document.getElementById("userEmail")
+    let userEmailVal = userEmail.value
+
+    let userPax = document.getElementById("userPax")
+    let userPaxVal = userPax.value
+
+    let userRemarks = document.getElementById("userRemarks")
+    let userRemarksVal = userRemarks.value
+
+    BookNow(userNameVal, userEmailVal, userPaxVal, userRemarksVal)
+})
+
+function BookNow(userName, userEmail, userPax, userRemarks) {
+    let url = 'https://api.sheety.co/b775bb08611d19542d4868561e1e3665/salonApp/sheet1';
+    let body = {
+        booking: {
+            name: userName,
+            email: userEmail,
+            pax: userPax,
+            remarks: userRemarks
+        }
+    }
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then((response) => response.json())
+        .then(json => {
+            // Do something with object
+            console.log(json.booking);
+            alert(json.booking.name + " added in the list!")
+        });
+}
